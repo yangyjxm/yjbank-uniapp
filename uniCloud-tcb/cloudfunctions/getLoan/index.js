2b99039c313bf1e3dde7e6fec7fe2c3aca6da994
杨雨佳 <yangyjxm@foxmail.com>
2022-07-01 22:32:04 +0800

    'merge'

---
+++ uniCloud-tcb/cloudfunctions/getLoan/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const db = uniCloud.database().collection('loan_info')
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	let data = await db.orderBy("createTime", "asc").get()
+	//返回数据给客户端
+	return data
+};
